576e2c0039b0be36e855bb81f0e4889ef4710d98
getoar <getoar@coda.tech>
2023-05-31 10:06:37 +0200

    Document Migrator
    Fixes

--- src/OrderDocumentMigrator/OrderDocumentMigratorConfig.php
+++ src/OrderDocumentMigrator/OrderDocumentMigratorConfig.php
@@ -4,14 +4,14 @@ namespace App\OrderDocumentMigrator;
 
 interface OrderDocumentMigratorConfig
 {
-    public const EXPORTER_URL = 'https://sw5.elektro-bruhn.de/shop/api';
-    public const EXPORTER_KEY = 'ntfrmc';
-    public const EXPORTER_SECRET = '3a8MIAfpB9nPn51VV1vpALJKQ33SvemfBCR70IDL';
 
 
-    public const RECEIVER_URL = 'http://192.168.178.172:8080/api';
-    public const RECEIVER_KEY = 'SWIAD0DXB3HUUMFWVUFJN3PRDA';
-    public const RECEIVER_SECRET = 'MnBKUTJOaWVQeVFETUZ0WVZQYnBGZVJTYXlQVVh4Y2Z4WllOa3M';
+
+//    public const RECEIVER_URL = 'http://192.168.178.172:8080/api';
+//    public const RECEIVER_KEY = 'SWIAYXH5MEFVN0S4TVRYVNBRZA';
+//    public const RECEIVER_SECRET = 'd2JTZHZsdTZmbXh1MVp0c1UwU1lNWGtvc1BUQVptU2NrSHVNV0Y';
+
+
 
     public const EXPORTER_ACCESS_GRANT = [
         'url' => self::EXPORTER_URL,
